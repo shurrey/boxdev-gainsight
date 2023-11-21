0a4c8fb80ebd10b918dff513c753893b3a30962d
Scott Hurrey <scott.hurrey@gmail.com>
2023-11-21 11:55:21 -0500

    test

--- bodev-gainsight.js
+++ bodev-gainsight.js
@@ -1,3 +1,4 @@
-if(inSidedData.page.url === "p/developer" || inSidedData.page.url === "developer-forum-42" || inSidedData.page.url === "box-platform-57" || inSidedData.page.url === "platform-advisory-board-56" || inSidedData.page.url === "announcements-58") {
+if(inSidedData.page.url === "p/developer" || inSidedData.page.url === "developer-forum-42" || inSidedData.page.url === "box-platform-57" || inSidedData.page.url === "platform-advisory-board-56" || inSidedData.page.url === "announcements-58")
+{
     jQuery('.header-navigation_logo').attr('src', 'https://uploads-us-west-2.insided.com/box-en/attachment/c4ebf7f5-1a4d-45d5-99d9-145d55969eb4.png')};
-}
+};
